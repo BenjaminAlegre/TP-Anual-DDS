fbf26db15f5b88e66e7f121c7af8013d34656c37
Benjamin Alegre <81981001+BenjaminAlegre@users.noreply.github.com>
2023-10-20 22:34:26 -0300

    Create TestEntidades.java

---
+++ src/test/java/TestEntidades.java
@@ -0,0 +1,13 @@
+import model.repositorios.RepositorioEntidades;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+
+public class TestEntidades {
+
+    private static EntityManagerFactory emf;
+    private static EntityManager em;
+
+    RepositorioEntidades repositorioEntidades = new RepositorioEntidades();
+
+}
